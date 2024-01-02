d4f40e828522016a07482fbde31f3f5916a88bc6
HustleWithAbhimanyu <hustlewithabhimanyu@gmail.com>
2024-01-02 21:29:35 +0530

    Made the app responsive for mobile

--- src/components/Login.js
+++ src/components/Login.js
@@ -107,7 +107,7 @@ const Login =() => {
         <div>
           <Header/>
             <div className="absolute">
-                <img 
+                <img className="h-screen object-cover"
                 src={BG_URL}
                 alt="Logo"
                 />
@@ -116,7 +116,7 @@ const Login =() => {
                 onSubmit={(e) => {
                     e.preventDefault();
                   }}
-                className="w-3/12 absolute p-12 my-36 mx-auto left-0 right-0 bg-black text-white rounded-lg bg-opacity-80">
+                  className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
                 <h1 className="font-bold text-3xl py-4">{isSignInForm ?"Sign In":"Sign Up"}</h1>
                 {!isSignInForm && (
                 <input
